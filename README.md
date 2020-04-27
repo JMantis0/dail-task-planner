e84dc554f46d38bac49028afe6864c8734268106
JMan <60026263+JMantis0@users.noreply.github.com>
2020-04-27 05:43:15 -0700

    second commit.  HTML js css and readme complete

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-## Day Planner
+##Day Planner
 
 Keep your tasks organized with this simple day planner.
 
-Utilizes the moment.js API to color code your tasks based on the current time of the day.
+**[Try it yourself!](https://jmantis0.github.io/daily-task-planner/)**
+
+## Features
+
+**Dynamic Appearance** 
+This planner utilizes the moment.js API to dynamically detect the time of day and adjust the planner's appearance automatically, letting you know where you're at in your day.
+
+**Persistent Data Storage**  
+This planner also allows you to edit, save, or delete your tasks as you wish, and your data will persist and remain available even if you close or refresh your browser!
+
+**It has a Digital Clock**
+Time is money!
+
+## Screenshots
+
+![empty](assets/images/scheduler.PNG)
+
+![color-coding](assets/images/color-coded.PNG)
+
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,100 @@
+//  Global variable
+let taskList = ["","","","","","","","",""];
+
+$(document).ready(function () {
+
+	sounds = {
+		wetclick: () => $("#wetclick")[0].play()
+	}
+
+	//  Function start displays the current date and time in the header.
+	//  It also sets an interval to update the clock and time-block colors.
+	function start() {
+
+		$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+		
+		setInterval(function() {
+
+			$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+			
+		}, 1000);
+
+		renderTasks();
+		updateColors();
+
+		setInterval(updateColors, 60000);
+
+	}
+
+	//  Function renderTasks gets data from localStorage and places it in 
+	//  the proper textareas.
+	function renderTasks() {
+
+		taskList = JSON.parse(localStorage.getItem("tasks"));
+
+		for(let i = 0; i < 9; i++) {
+
+			$("#" + (i+9) + " textarea").text(taskList[i]);
+
+		}
+
+	}
+		
+	//  Function updateColors applies colors to textareas based on
+	//  current time of day.
+	function updateColors() {
+
+		$(".row").each(function(){
+
+			let currentHour = 13; //parseInt(moment().format("H"));
+			let textareaHour = parseInt($(this, "textarea").attr("id"));
+
+			if(textareaHour < currentHour) {
+				
+				$(this, "textarea").addClass("past");
+
+			}
+			else if (textareaHour > currentHour) {
+
+				$(this, "textarea").addClass("future");
+			}
+			else {
+
+				$(this, "textarea").addClass("present");
+
+			}
+
+		});
+
+	}
+
+	//  Function saveTask saves the hourly task for whichever
+	//  button was clicked into the taskList and localStorage
+	function saveTask() {
+
+		let textHour = $(this).parents(".row").attr("id");
+		let hourTask = $("#" + textHour + " textarea").val();
+		taskList[textHour-9] = hourTask;
+		localStorage.setItem("tasks", JSON.stringify(taskList));
+		
+	}
+
+	//  Spinning is fun.
+	function oneSpin() {
+
+		let a = $(this).find("i");
+		let b = $("#" + parseInt($(this).parents(".row").attr("id")) + " p");
+		a.addClass("spin");
+		b.addClass("spin");
+		setTimeout(function(){
+			a.removeClass("spin");
+			b.removeClass("spin");
+		 },1000);
+
+	}
+
+	$(".saveBtn").click(saveTask).click(sounds.wetclick).click(oneSpin);
+	$(".row").last().css("margin-bottom", "100px");
+	start();
+
+});
